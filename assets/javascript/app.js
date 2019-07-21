b4965c89154555839887bd0c7982ef2a33de42a5
Noel Cruz <noel.j.cruz@outlook.com>
2019-07-21 14:54:51 -0700

    Created initial set of buttons.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,9 @@
 
 var sportsArray = ["baseball", "soccer", "football", "dodgeball", "karate", "yoga", "hockey", "running", "dance", "swimming", "golf", "tennis", "nascar", "boxing", "basketball", "rugby", "cycling", "diving", "wrestling", "gymnastics"];
+
+for (var i = 0; i < sportsArray.length; i++) {
+    var sport = sportsArray[i];
+    var sportButton = $("<button>");
+    sportButton.text(sport);
+    $("#button-area").append(sportButton);
+}

2d025cc3fc52194cfaffde63558340a9836434fd
Noel Cruz <noel.j.cruz@outlook.com>
2019-07-22 23:39:48 -0700

    Added some styling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,31 +17,47 @@ function displayGifs() {
     var sport = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         sport + "&api_key=sMem5S8j5UGqlpr1gWf3ySnwoqJZXMmt&limit=10";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
         .then(function (response) {
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
                 var sportDiv = $("<div>");
                 var p = $("<p>").text("Rating: " + results[i].rating);
                 var sportImage = $("<img>");
-                sportImage.attr("src", results[i].images.fixed_height.url);
+                sportImage.attr("src", results[i].images.fixed_height_still.url);
+                sportImage.attr("data-still", results[i].images.fixed_height_still.url);
+                sportImage.attr("data-animate", results[i].images.fixed_height.url);
+                sportImage.attr("data-state", "still");
+                sportImage.addClass("gif");
                 sportDiv.append(p);
                 sportDiv.append(sportImage);
                 $("#gif-area").prepend(sportDiv);
             }
         });
-    }
+}
 
-    $("#add-sport").on("click", function (event) {
-        event.preventDefault();
-        newsport = $("#new-sport").val().trim();
-        sportsArray.push(newsport);
-        createButtons();
-    });
-    
+$("#gif-area").on("click", ".gif", function (event) {
+    event.preventDefault();
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
+
+$("#add-sport").on("click", function (event) {
+    event.preventDefault();
+    newsport = $("#new-sport").val().trim();
+    sportsArray.push(newsport);
     createButtons();
-    
-    $(document).on("click", ".sport", displayGifs);
+});
+
+createButtons();
+
+$(document).on("click", ".sport", displayGifs);

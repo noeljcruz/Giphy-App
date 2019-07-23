abc35b75807de05c551e55807ee26adef35450c7
Noel Cruz <noel.j.cruz@outlook.com>
2019-07-22 22:18:12 -0700

    Built function to append gifs and ratings to the DOM.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,35 @@ function createButtons() {
     }
 }
 
-createButtons();
+function displayGifs() {
+    var sport = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        sport + "&api_key=sMem5S8j5UGqlpr1gWf3ySnwoqJZXMmt&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var sportDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var sportImage = $("<img>");
+                sportImage.attr("src", results[i].images.fixed_height.url);
+                sportDiv.append(p);
+                sportDiv.append(sportImage);
+                $("#gif-area").prepend(sportDiv);
+            }
+        });
+    }
 
-$("#add-sport").on("click", function (event) {
-    event.preventDefault();
-    newsport = $("#new-sport").val().trim();
-    sportsArray.push(newsport);
+    $("#add-sport").on("click", function (event) {
+        event.preventDefault();
+        newsport = $("#new-sport").val().trim();
+        sportsArray.push(newsport);
+        createButtons();
+    });
+    
     createButtons();
-});
+    
+    $(document).on("click", ".sport", displayGifs);
